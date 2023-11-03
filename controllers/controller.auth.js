ea56924e0124a460f295afdf0a099cdddbaab72e
Jorge Rivera <jorge.rivera@ssde.com.mx>
2023-11-02 22:26:40 -0600

    Revert "SSDE-06. Fixed create token process, added object to attach functions to refreshToken object to create and verify token validity. Removed close connection after initialize DB"
    This reverts commit 0bfe62e427748c6b2ce772ef3661a14f59cb25f6.

--- controllers/controller.auth.js
+++ controllers/controller.auth.js
@@ -2,10 +2,10 @@ const db = require("../models");
 const config = require("../config/auth.config");
 const User = db.users;
 const Role = db.roles;
-const RefreshToken = db.token;
+const RefreshToken = db.refreshToken;
 // const Op = db.Sequelize.Op;
-let jwt = require("jsonwebtoken");
-let bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
 
 exports.signup = (req, res) => {
   // Save User to Database
@@ -16,18 +16,22 @@ exports.signup = (req, res) => {
   })
     .then(user => {
       if (req.body.roles) {
-          Role.findOne({ "name": req.body.roles }).then(roles => {
-              user.roles.push(roles);
-              user.save();
-              res.send({ message: "User was registered successfully!" });
+        Role.findAll({
+          where: {
+            name: {
+              [Op.or]: req.body.roles
+            }
+          }
+        }).then(roles => {
+          user.setRoles(roles).then(() => {
+            res.send({ message: "User was registered successfully!" });
           });
+        });
       } else {
         // user role = 1
-          Role.findOne({"name":"user"}).then(role => {
-              user.roles.push(role);
-              user.save();
-              res.send({ message: "User was registered successfully!" });
-          })
+        user.setRoles([1]).then(() => {
+          res.send({ message: "User was registered successfully!" });
+        });
       }
     })
     .catch(err => {
@@ -37,12 +41,9 @@ exports.signup = (req, res) => {
 
 exports.signin = (req, res) => {
   console.log('request body: ',req.body);
-  // db.connect();
-  User.findOne({ "username": req.body.username }).populate('roles')
+  db.connect();
+  User.findOne({ "username": req.body.username })
     .then( async (user) => {
-        // await user.populate('roles');
-        console.log('User found: ', user.username);
-        console.log('roles: ', user.roles);
         if (!user) {
             return res.status(404).send({ message: "User Not found." });
         }
@@ -50,7 +51,6 @@ exports.signin = (req, res) => {
             req.body.password,
             user.password
         );
-        // console.log('password matches: ', passwordIsValid);
         if (!passwordIsValid) {
             return res.status(401).send({
             accessToken: null,
@@ -61,24 +61,23 @@ exports.signin = (req, res) => {
             // expiresIn: 86400 // 24 hours
             expiresIn: config.jwtExpiration//1200 // 20 minutes
         });
-        // console.log(RefreshToken);
         let refreshToken = await RefreshToken.createToken(user);
-        // console.log('refresh token: ',refreshToken);
         let authorities = [];
-        user.roles.forEach(role => {
-           authorities.push("ROLE_" + role.name);// name.toUpperCase());
-        });
-        res.status(200).send({
+        user.getRoles().then(roles => {
+            for (let i = 0; i < roles.length; i++) {
+            authorities.push("ROLE_" + roles[i].name.toUpperCase());
+            }
+            res.status(200).send({
             id: user.id,
             username: user.username,
             email: user.email,
             roles: authorities,
             accessToken: token,
             refreshToken: refreshToken
+            });
         });
     })
     .catch(err => {
-        console.log('error: ',err);
         res.status(500).send({ message: err.message });
     });
 };
@@ -89,21 +88,21 @@ exports.refreshToken = async (req, res) => {
     return res.status(403).json({ message: "Refresh Token is required!" });
   }
   try {
-    let refreshToken = await RefreshToken.findOne( {"token": requestToken } );
+    let refreshToken = await RefreshToken.findOne({ where: { token: requestToken } });
     // console.log(refreshToken)
     if (!refreshToken) {
       res.status(403).json({ message: "Refresh token is not in database!" });
       return;
     }
     if (RefreshToken.verifyExpiration(refreshToken)) {
-      await RefreshToken.delete( { "_id": refreshToken.id  });
+      RefreshToken.destroy({ where: { id: refreshToken.id } });
       
       res.status(403).json({
         message: "Refresh token was expired. Please make a new signin request",
       });
       return;
     }
-    const user = await refreshToken.user;
+    const user = await refreshToken.getUser();
     let newAccessToken = jwt.sign({ id: user.id }, config.secret, {
       expiresIn: config.jwtExpiration,
     });
--- models/model.refreshToken.js
+++ models/model.refreshToken.js
@@ -11,40 +11,12 @@ module.exports = mongoose => {
         expiryDate: Date
     });
 
-    // refreshTokenSchema.method( "createToken" , (user) => {
-    //     let expiredAt = new Date();
-    //     expiredAt.setSeconds(expiredAt.getSeconds() + config.jwtRefreshExpiration);
-    //     let _token = uuidv4();
-    //     // return refreshTokenlet refreshToken = await this.create({
-    //     return this.create({
-    //         token: _token,
-    //         userId: user,
-    //         expiryDate: expiredAt.getTime(),
-    //     }).then( result => {
-    //         return result.token;
-    //     });
-    // });
-
-    // refreshTokenSchema.method( "verifyExpiration", (token) => {
-    //     let expiryDate = new Date(token.expiryDate);
-    //     return expiryDate.getTime() < new Date().getTime();
-    // });
-
-    refreshTokenSchema.method("toJSON", () => {
-        const { __v, _id, ...object } = this.toObject();
-        object.id = _id;
-        return object;
-    });
-
-    let tokenFactory = {}
-
-    tokenFactory.dbObject = new mongoose.model("refreshToken", refreshTokenSchema);
-    tokenFactory.createToken = (user) => {
+    refreshTokenSchema.createToken = (user) => {
         let expiredAt = new Date();
         expiredAt.setSeconds(expiredAt.getSeconds() + config.jwtRefreshExpiration);
         let _token = uuidv4();
         // return refreshTokenlet refreshToken = await this.create({
-        return tokenFactory.dbObject.create({
+        return this.create({
             token: _token,
             userId: user,
             expiryDate: expiredAt.getTime(),
@@ -52,15 +24,17 @@ module.exports = mongoose => {
             return result.token;
         });
     };
-    tokenFactory.verifyExpiration = (token) => {
+
+    refreshTokenSchema.verifyExpiration = (token) => {
         let expiryDate = new Date(token.expiryDate);
         return expiryDate.getTime() < new Date().getTime();
     };
-    tokenFactory.toJSON = () => {
-        const { __v, _id, ...object } = this.dbObject.toObject();
+
+    refreshTokenSchema.method("toJSON", () => {
+        const { __v, _id, ...object } = this.toObject();
         object.id = _id;
         return object;
-    };
+    });
 
-    return tokenFactory;
+    return new mongoose.model("refreshToken", refreshTokenSchema);
 }
